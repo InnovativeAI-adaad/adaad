28c93c99cd00ce7c2dfba109d8b4ad9ebbfad447
Dustin Reid <dustinreid82@gmail.com>
2026-02-26 22:05:10 -0600

    Create runtime/constants.py with Aponi port and URL constants

---
+++ runtime/constants.py
@@ -0,0 +1,7 @@
+# Constants for Aponi Runtime
+
+# Canonical Aponi port
+APONI_PORT = 8080
+
+# Canonical Aponi URL
+APONI_URL = 'http://localhost:{}/'  # You can replace localhost with the actual host if needed
